115121db5c3a3943b220f8240bc5bdd5fbd72204
Moshko Alexei <prestigio458@gmail.com>
2019-10-08 09:31:11 +0300

    change name of variable trainNum

--- src/by/epam/classes/Train.java
+++ src/by/epam/classes/Train.java
@@ -8,17 +8,17 @@ package by.epam.classes;
 
 public class Train {
     private String dest;
-    private String trainNum;
+    private String num;
     private String departTime;
 
     public Train() {
         dest = "None";
-        trainNum = "None";
+        num = "None";
         departTime = "00:00";
     }
-    public Train(String dest, String trainNum, String departTime) {
+    public Train(String dest, String num, String departTime) {
         this.dest = dest;
-        this.trainNum = trainNum;
+        this.num = num;
         this.departTime = departTime;
     }
 
@@ -26,8 +26,8 @@ public class Train {
         this.dest = dest;
     }
 
-    public void setTrainNum(String trainNum) {
-        this.trainNum = trainNum;
+    public void setNum(String num) {
+        this.num = num;
     }
 
     public void setDepartTime(String departTime) {
@@ -38,8 +38,8 @@ public class Train {
         return dest;
     }
 
-    public String getTrainNum() {
-        return trainNum;
+    public String getNum() {
+        return num;
     }
 
     public String getDepartTime() {
@@ -52,7 +52,7 @@ public class Train {
         while(!isSorted) {
             isSorted = true;
             for (int i = 0; i < trains.length-1; i++) {
-                if(Integer.parseInt(trains[i].trainNum) > Integer.parseInt(trains[i+1].trainNum)){
+                if(Integer.parseInt(trains[i].num) > Integer.parseInt(trains[i+1].num)){
                     isSorted = false;
                     buf = trains[i];
                     trains[i] = trains[i+1];
@@ -63,12 +63,12 @@ public class Train {
         return trains;
     }
 
-    public static void printByNum(Train[] trains, String trainNum) {
+    public static void printByNum(Train[] trains, String num) {
         int flag = 1;
         for (int i = 0; i < trains.length; i++) {
-            if(trains[i].trainNum == trainNum) {
+            if(trains[i].num == num) {
                 flag = 0;
-                System.out.println("Train to " + trains[i].dest + " #" + trains[i].trainNum + " departed at " + trains[i].departTime);
+                System.out.println("Train to " + trains[i].dest + " #" + trains[i].num + " departed at " + trains[i].departTime);
             }
         }
         if(flag == 1) {
